567f9cb3edfd30f82cbbcee7fcd3f40c0a9e6178
muhammedsaidckr <muhammedsaidckr@gmail.com>
2025-08-10 02:18:02 +0300

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { I18nProvider } from "@/components/I18nProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +44,7 @@ export default function RootLayout({
       >
         <I18nProvider>
           {children}
+          <Analytics />
         </I18nProvider>
       </body>
     </html>
